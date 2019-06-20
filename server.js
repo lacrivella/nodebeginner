3ade02b2a1f7943202fe8adbe3991eb235aade26
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-20 14:35:35 -0700

    fixed typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var http = required('http');
+var http = required("http");
 
 http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type":  "text/plain"});
     response.write("Hello World");
     response.end();
 }).listen(8888);
