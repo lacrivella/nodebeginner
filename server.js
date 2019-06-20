27e879f92df89b78eea8ee8a1bfa858ae0f11a97
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-20 15:06:11 -0700

    refactor pg 14

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require("http");
 
 function onRequest(request, response) {
-    response.writeHead(200, {"Content-Type":  "text/plain"});
+    console.log("request recieved");
+    response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
 }
 
 http.createServer(onRequest).listen(8888);
+
+console.log("server has started");
