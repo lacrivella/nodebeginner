26c3f352d0b47cc061fe2df87573d916f995c9c2
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-20 22:48:26 -0700

    fixed error found in router.js

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 function route(handle, pathname) {
     console.log("about to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname]();
+        return handle[pathname]();
     } else {
         console.log("no request handler found for " + pathname);
         return "404 not found";
