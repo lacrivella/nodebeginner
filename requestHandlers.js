a6d1f3ec4d2f0972a5d48014a5c0556fad435054
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-21 12:11:57 -0700

    pg 41 show image

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,5 @@
 var querystring = require("querystring");
+    fs = require("fs");
 
 function start(response, postData) {
     console.log("request handler start was called");
@@ -30,5 +31,12 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    response.writeHead(200, {"Content-Type": "image/png"});
+    fs.createReadStream("/tmp/test.png").pipe(response);
+}
+
 exports.start = start;
 exports.upolad = upload;
+exports.show = show;
