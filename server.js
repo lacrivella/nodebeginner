a34690adfff831bddb9dc14972aa389593c4b210
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-21 13:00:16 -0700

    fixing errors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("request for " + pathname + " received");
-        route(handle, pathname, response, postData);
+        route(handle, pathname, response, request);
     }
     
     http.createServer(onRequest).listen(8888);
