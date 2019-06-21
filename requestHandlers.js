7da05f1f38ce4c121373484eabbb810d70567399
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-21 11:55:42 -0700

    pg 37 completed post data

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
     console.log("request handler start was called");
     
 
@@ -22,10 +22,11 @@ function start(response) {
         response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("request handler upload was called");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("You've sent the text: "+
+        querystring.parse(postData).text);
     response.end();
 }
 
