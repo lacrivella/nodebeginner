afc0e798d08704045fb53c59dbfb0e39d6951ab2
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-21 11:13:47 -0700

    typos ugh

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 function route(handle, pathname, response) {
     console.log("about to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        return handle[pathname]();
+        return handle[pathname](response);
     } else {
         console.log("no request handler found for " + pathname);
         response.writeHead(404, {"Content-Type": "text/plain"});
