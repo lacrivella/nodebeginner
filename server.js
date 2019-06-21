90401ee77e5df5e4145f4f0369f9ef2c953ac179
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-21 12:58:11 -0700

    pg 46 refactor code to upload image

--- server.js
+++ server.js
@@ -3,21 +3,9 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("request for " + pathname + " received");
-
-        request.setEncoding("utf8");
-
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received POST data chuck '"+
-            postDataChunk + "'.");
-        });
-
-        request.addListener("end", function() {
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, postData);
     }
     
     http.createServer(onRequest).listen(8888);
